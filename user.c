2f3c67e91e800bbd78dcd6de1a10fde7f1a95588
jose.francisco.udaeta <jose.francisco.udaeta@est.fib.upc.edu>
2022-09-19 18:04:34 +0200

    [ADD] Añadido función add y usado en main

--- user.c
+++ user.c
@@ -4,12 +4,25 @@ char buff[24];
 
 int pid;
 
+int
+add (int par1, int par2)
+{
+  return par1 + par2;
+}
+
 int __attribute__ ((__section__(".text.main")))
   main(void)
 {
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
+  /*
+  > Modify the main() of the user.c file to add a call to the previous function with some dummy
+values (like 0x42 and 0x666) and store the resulting value in a local variable.
+  */
+ int sum_result;
+
+ sum_result = add (0x42, 0x666);
     
   while(1) { }
 }
