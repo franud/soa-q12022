c4bd3866aedc0dcd317693bb20b09b19ec68b854
carlos.rojas.pons <carlos.rojas.pons@estudiantat.upc.edu>
2022-09-30 01:17:29 +0200

    [ADD] call to write, to test if it's working properly

--- user.c
+++ user.c
@@ -5,11 +5,12 @@ char buff[24];
 int pid;
 
 int __attribute__ ((__section__(".text.main")))
-  main(void)
+    main(void)
 {
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
-    
-  while(1) { }
+	write(1, "hola", 5);
+
+    while(1) { }
 }
