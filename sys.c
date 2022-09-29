767f1185d488c6cce036c7b87fe9041e50041f35
carlos.rojas.pons <carlos.rojas.pons@estudiantat.upc.edu>
2022-09-29 20:49:22 +0200

    [ADD] sys_write in sys.c

--- sys.c
+++ sys.c
@@ -1,6 +1,7 @@
 /*
  * sys.c - Syscalls implementation
  */
+#include "errno.h"
 #include <devices.h>
 
 #include <utils.h>
@@ -21,6 +22,42 @@ int check_fd(int fd, int permissions)
   if (fd!=1) return -9; /*EBADF*/
   if (permissions!=ESCRIPTURA) return -13; /*EACCES*/
   return 0;
+}
+
+int sys_write(int fd, char * buffer, int size) {
+  int err = check_fd(fd, ESCRIPTURA);
+  if (err <= 0) return err; // if check_fd returned an error, return that same error.
+  if (buffer == NULL) return -EFAULT; // error out if buffer is null.
+  if (size < 1) return -EINVAL; // error out if there is nothing to write (size < 1).
+
+  int bytes_left = size;
+
+  int sys_buffer_size = 256;
+  char sys_buffer[sys_buffer_size];
+
+  // write in chunks that fit into our sys_buffer.
+  while (bytes_left > sys_buffer_size) {
+    int err = copy_from_user(buffer, sys_buffer, sys_buffer_size);
+    if (err != 0) return -EIO; // check for errors copying data.
+
+    int bytes_written = sys_write_console(sys_buffer, sys_buffer_size);
+
+    bytes_left -= bytes_written;
+    buffer += bytes_written;
+  }
+  
+  // write only the remaining bytes.
+  if ( bytes_left > 0 ) {
+    int err = copy_from_user(buffer, sys_buffer, bytes_left);
+    if (err != 0) return -EIO; // check for errors copying data.
+
+    int bytes_written = sys_write_console(sys_buffer, bytes_left);
+
+    bytes_left -= bytes_written;
+    buffer += bytes_written;
+  }
+  
+  return size;
 }
 
 int sys_ni_syscall()
@@ -45,3 +82,4 @@ int sys_fork()
 void sys_exit()
 {  
 }
+
