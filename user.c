8bd09f2c0d4b7709d2502ff1457011d2068084ab
carlos.rojas.pons <carlos.rojas.pons@estudiantat.upc.edu>
2022-09-30 21:06:58 +0200

    [ADD] added test for gettime

--- user.c
+++ user.c
@@ -1,7 +1,7 @@
 #include <libc.h>
 #include <types.h>
 
-char buff[24];
+char buff[32];
 
 int pid;
 
@@ -9,13 +9,13 @@ int pid;
 // TEST 1: Escribir Hola en pantalla.
 void test_write1() {
     write(1, "\n", 1);
-    write(1, "hola", 5);
+    write(1, "hola\n", 5);
 }
 
 // TEST 2: Forzar error: fd != 1.
 void test_write2() {
     write(1, "\n", 1);
-    write(2, "hola", 5);
+    write(2, "hola\n", 5);
     perror();
 }
 
@@ -30,8 +30,15 @@ void test_write3() {
 // TEST 4: Forzar error: size < 0.
 void test_write4() {
     write(1, "\n", 1);
-    write(2, "hola", -1);
+    write(2, "hola\n", -1);
     perror();
+}
+
+// gettime test: print time elapsed.
+void test_gettime() {
+    itoa(gettime(), buff);
+    write(1, buff, strlen(buff));
+    write(1, "\n", 1);
 }
 
 int __attribute__ ((__section__(".text.main")))
@@ -39,10 +46,13 @@ int __attribute__ ((__section__(".text.main")))
 {
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
+
     test_write1();
     //test_write2();
     //test_write3();
     //test_write4();
 
-    while(1) { }
+    while(1) {
+        //test_gettime();
+    }
 }
