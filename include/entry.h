6bf1163eb549c96f765631b96bc3304e4dceba85
carlos.rojas.pons <carlos.rojas.pons@estudiantat.upc.edu>
2022-09-30 20:36:45 +0200

    [ADD] function headers for entry.S

--- include/entry.h
+++ include/entry.h
@@ -5,8 +5,12 @@
 #ifndef __ENTRY_H__
 #define __ENTRY_H__
 
+void clock_handler();
 void keyboard_handler();
 void system_call_handler();
 void syscall_handler_sysenter();
+
+// Write a value to the desired MSR position.
+void writeMSR(unsigned int msr_position, unsigned int value);
 
 #endif  /* __ENTRY_H__ */
